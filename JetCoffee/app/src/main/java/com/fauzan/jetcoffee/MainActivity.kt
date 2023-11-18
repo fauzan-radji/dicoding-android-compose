f00b02eeeb951f236e7629da8a6140c27d8ce635
Fauzan Radji <72055502+fauzan-radji@users.noreply.github.com>
2023-11-18 20:42:07 +0800

    Layout pada Compose: Latihan Membuat Kategori

--- JetCoffee/app/src/main/java/com/fauzan/jetcoffee/MainActivity.kt
+++ JetCoffee/app/src/main/java/com/fauzan/jetcoffee/MainActivity.kt
@@ -4,15 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.fauzan.jetcoffee.components.CategoryItem
 import com.fauzan.jetcoffee.components.Search
+import com.fauzan.jetcoffee.components.SectionText
+import com.fauzan.jetcoffee.model.dummyCategory
 import com.fauzan.jetcoffee.ui.theme.JetCoffeeTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +38,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JetCoffeeApp() {
-    Banner()
+    Column {
+        Banner()
+        SectionText(title = stringResource(id = R.string.section_category))
+        CategoryRow()
+    }
 }
 
 @Composable
@@ -47,10 +60,30 @@ fun Banner(
     }
 }
 
+@Composable
+fun CategoryRow() {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp)
+    ) {
+        items(dummyCategory, key = { it.textCategory }) { category ->
+            CategoryItem(category = category)
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetCoffeeTheme {
         JetCoffeeApp()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CategoryRowPreview() {
+    JetCoffeeTheme {
+        CategoryRow()
     }
 }
